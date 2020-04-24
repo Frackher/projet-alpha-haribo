3c8ea746982ece96aa205aa7e88de20c6c3ecfdb
cedricN <cedric.niemenck@gmail.com>
2020-04-24 09:23:27 +0200

    Modification, ajout de fonction

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,3 +64,15 @@
 				});
 
 })(jQuery);
+
+var btnPopup = document.getElementById('btnPopup');
+var overlay = document.getElementById('overlay');
+btnPopup.addEventListener('click',openMoadl);
+function openMoadl() {
+	overlay.style.display='inline';
+}
+var btnClose = document.getElementById('btnClose');
+btnClose.addEventListener('click',closeModal);
+function closeModal() {
+	overlay.style.display='none';
+}
